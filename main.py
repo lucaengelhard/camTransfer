fcef15420eb94cd89e76e0fcde59faffa3b88643
Luca Engelhard <me@lucaengelhard.com>
2026-01-24 11:34:31 +0100

    first steps

---
+++ main.py
@@ -0,0 +1,88 @@
+import gphoto2 as gp
+import time
+import os
+import sys
+
+def main():
+    # Set up dir
+    save_dir = os.path.join(os.getcwd(), 'images')
+    os.makedirs(save_dir, exist_ok=True)
+
+    camera = connect_camera()
+    """     timeout = 3000  # milliseconds
+    while True:
+        try:
+            event_type, event_data = camera.wait_for_event(timeout)
+            if event_type == gp.GP_EVENT_FILE_ADDED:
+                cam_file = camera.file_get(
+                    event_data.folder, event_data.name, gp.GP_FILE_TYPE_NORMAL)
+                target_path = os.path.join(os.getcwd(), event_data.name)
+                print("Image is being saved to {}".format(target_path))
+                cam_file.save(target_path)
+        except gp.GPhoto2Error as ex:
+            print(f"Camera error: {ex}. Attempting to reconnect...")
+            try:
+                camera.exit()
+            except Exception:
+                pass
+
+            time.sleep(2)
+            camera = connect_camera() """
+    poll_image(timeout=3000, camera=camera)
+
+    return 0
+
+def poll_image(timeout: int, camera: gp.Camera):
+    while True:
+        print(f"Waiting for image (timeout: {timeout})")
+        try:
+            event_type, event_data = camera.wait_for_event(timeout)
+            if event_type == gp.GP_EVENT_FILE_ADDED:
+                cam_file = camera.file_get(event_data.folder, event_data.name, gp.GP_FILE_TYPE_NORMAL)
+                target_path = os.path.join(os.getcwd(), event_data.name)
+                print("Image is being saved to {}".format(target_path))
+                cam_file.save(target_path)
+
+        except gp.GPhoto2Error as ex:
+            print(f"Camera error: {ex}. Attempting to reconnect...")
+            try:
+                camera.exit()
+            except Exception:
+                pass
+
+            time.sleep(2)
+            camera = connect_camera()
+        
+
+def connect_camera() -> gp.Camera:
+    print('Please connect and switch on your camera...')
+    camera = gp.Camera()
+
+    while True:
+        try:
+            camera.init()
+        except gp.GPhoto2Error as ex:
+            if ex.code == gp.GP_ERROR_MODEL_NOT_FOUND:
+                # no camera, try again in 2 seconds
+                time.sleep(2)
+                continue
+            # some other error we can't handle here
+            raise
+        # operation completed successfully so exit loop
+        break
+
+    
+
+    print(f"Camera {get_camera_name(camera)} initialized successfully")
+
+    return camera
+
+def get_camera_name(camera: gp.Camera) -> str:
+    summary = str(camera.get_summary())
+    model_line = [line for line in summary.split('\n') if 'Model:' in line][0]
+    camera_model = model_line.split('Model:')[1].strip()
+
+    return camera_model
+
+if __name__ == "__main__":
+    sys.exit(main())
